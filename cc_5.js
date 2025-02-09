bf71c832a868afd453372c7148802c513f71ef37
Gmailvicente04 <vicente.oswald@gmail.com>
2025-02-09 15:47:07 -0500

    "Task 1 - Customer Profile."

---
+++ cc_5.js
@@ -0,0 +1,14 @@
+// Task 1: Object Properties
+
+// Declare the customer object
+const customer = {
+    name: "John Doe",
+    age: 35,
+    email: "john.doe@email.com"
+};
+
+// Log each property using console.log() and a template literal
+console.log(`Name: ${customer.name}`);
+console.log(`Age: ${customer.age}`);
+console.log(`Email: ${customer.email}`);
+
